704a95ac1f0c9088d809c2692475834d16d47d18
luchodeco <72599025+louisdeco@users.noreply.github.com>
2025-02-05 10:40:07 +0400

    feat: add button to refresh gif content
    - Add change button to HTML structure
    - Implement click handler to fetch new gif

--- src/gif-api-practice.js
+++ src/gif-api-practice.js
@@ -10,3 +10,18 @@ fetch(
   .then(function (response) {
     img.src = response.data.images.original.url;
   });
+
+const changeBtn = document.querySelector("button");
+
+changeBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=ox5NsOblwMxXaqqugyKUjZ4fYGoi4ABB&s=cats",
+    { mode: "cors" },
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+});
