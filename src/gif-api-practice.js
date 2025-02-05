80912132bf8da07e69608517eae92ff495e51326
luchodeco <72599025+louisdeco@users.noreply.github.com>
2025-02-05 10:31:13 +0400

    feat: implement basic giphy api integration
    - Add fetch call to Giphy translate endpoint
    - Set image source from API response

---
+++ src/gif-api-practice.js
@@ -0,0 +1,12 @@
+const img = document.querySelector("img");
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=ox5NsOblwMxXaqqugyKUjZ4fYGoi4ABB&s=cats",
+  { mode: "cors" },
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
